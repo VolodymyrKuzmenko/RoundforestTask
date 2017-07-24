9144ddb7eb1bc5d0df2d9c79a3a812e7258235d4
VolodymyrKuzmenko <kuzmenko.volodimir@gmail.com>
2017-07-25 02:12:24 +0300

    reffactoring

--- src/main/java/roundforest/AmazonReviewService.java
+++ src/main/java/roundforest/AmazonReviewService.java
@@ -18,34 +18,19 @@ import static roundforest.ReviewFields.PROFILE_NAME;
 public class AmazonReviewService {
 
     public Iterable<String> findMostActiveUsers(File reviews, int maxItems) throws IOException {
-        Map<String, Integer> map = new HashMap<>();
-        Reader in = new FileReader(reviews);
-        Iterable<CSVRecord> records = DEFAULT.withHeader().parse(in);
-        StreamSupport.stream(records.spliterator(), false)
-                .map(x -> x.get(PROFILE_NAME))
-                .forEach(x -> {
-                    Integer count = map.get(x);
-                    if (count == null) {
-                        map.put(x, 1);
-                    } else {
-                        map.put(x, ++count);
-                    }
-                });
-
-        return map.entrySet()
-                .stream()
-                .sorted((o1, o2) -> Integer.compare(o2.getValue(), o1.getValue()))
-                .limit(maxItems)
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
+        return findMostUsingItemInFile(reviews, maxItems, PROFILE_NAME);
     }
 
     public Iterable<String> findMostCommentedProduct(File reviews, int maxItems) throws IOException {
+        return findMostUsingItemInFile(reviews, maxItems, PRODUCT_ID);
+    }
+
+    private Iterable<String> findMostUsingItemInFile(File reviews, int maxItems, ReviewFields field) throws IOException {
         Map<String, Integer> map = new HashMap<>();
         Reader in = new FileReader(reviews);
         Iterable<CSVRecord> records = DEFAULT.withHeader().parse(in);
         StreamSupport.stream(records.spliterator(), false)
-                .map(x -> x.get(PRODUCT_ID))
+                .map(x -> x.get(field))
                 .forEach(x -> {
                     Integer count = map.get(x);
                     if (count == null) {
