3d2956882e06ecf775dc03e781c71a22a7fc50cb
Volodymyr Kuzmenko <kuzmenko.volodimir@gmail.com>
2017-07-25 17:09:21 +0300

    star implement of translation

--- src/main/java/roundforest/TranslateService.java
+++ src/main/java/roundforest/TranslateService.java
@@ -1,5 +1,47 @@
 package roundforest;
 
+import org.springframework.web.client.RestTemplate;
+import roundforest.domain.ReviewDTO;
+import roundforest.mock.MockRestTemplate;
+
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static org.springframework.web.util.UriComponentsBuilder.fromHttpUrl;
+import static roundforest.domain.Languages.EN;
+import static roundforest.domain.Languages.FR;
+
 public class TranslateService {
 
+    private static final int MAX_SIZE = 1000;
+    private final String TRANSLATE_ENDPOINT = "https://api.google.com/translate";
+    private RestTemplate template = new MockRestTemplate();
+
+    public Iterable<String> translateReviews(Iterable<String> review) {
+        return StreamSupport.stream(review.spliterator(), false)
+                .map(this::translate)
+                .collect(Collectors.toList());
+    }
+
+    private String translate(String text) {
+        if (text.length() >= MAX_SIZE) {
+            return splitAndTranslate(text);
+        }
+        return translateData(text);
+
+    }
+
+    private String translateData(String text) {
+        return template.postForObject(
+                fromHttpUrl(TRANSLATE_ENDPOINT).build().encode().toUri(),
+                new ReviewDTO(EN, FR, text),
+                String.class);
+    }
+
+    private String splitAndTranslate(String text) {
+        String[] sentences = text.split("(?<=[a-z])\\.\\s+");
+        for (String sentence : sentences){
+            
+        }
+    }
 }
--- src/main/java/roundforest/domain/ReviewDTO.java
+++ src/main/java/roundforest/domain/ReviewDTO.java
@@ -1,11 +1,13 @@
 package roundforest.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.annotate.JsonPropertyOrder;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 @JsonPropertyOrder({"input_lang", "output_lang", "text"})
 public class ReviewDTO {
